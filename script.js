44fdee7f974f904bebbf6a95de99161c33799cb8
Goodness <goodnessabisoye@gmail.com>
2024-01-01 23:30:37 +0100

    JavaScript!

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
 function getComputerChoice() {
-	var choice = ["Rock", "paper", "scissors"];
+	var choice = ["rock", "paper", "scissors"];
 	let x = Math.floor(Math.random() * 3);
-
-	console.log(x);
-	console.log(choice[x]);
 
 	return (choice[x]);
 }
 
-let i = 0;
-
-while (i <= 10) {
-	getComputerChoice();
-	i++;
+function playRound(playerSelection, computerSelection) {
 }
