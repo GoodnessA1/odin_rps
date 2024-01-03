8345d0ec43feb2bc1b886e53d46297970c5d6fbb
Goodness <goodnessabisoye@gmail.com>
2024-01-03 23:49:06 +0100

    JavaScript is added

--- script.js
+++ script.js
@@ -5,5 +5,39 @@ function getComputerChoice() {
 	return (choice[x]);
 }
 
+var playerScore = 0;
+var computerScore = 0;
+
 function playRound(playerSelection, computerSelection) {
+	if (playerSelection == computerSelection) {
+		console.log(`It is a tie, please play again`);
+		return (2);
+	}
+
+	else if ((computerSelection == "rock") && (playerSelection == "paper") || (computerSelection == "scissors") && (playerSelection == "rock") || (computerSelection == "paper") && (playerSelection == "scissors"))
+	{
+		playerScore++;
+		console.log(`You: ${playerScore} , computer: ${computerScore}`);
+		return (1);
+	}
+
+	else {
+		computerScore++;
+		console.log(`You: ${playerScore} , computer: ${computerScore}`);
+		return (0);
+	}
+}
+
+var inc = 0;
+
+while (inc < 5) {
+	var playerSelection = (prompt("Enter Your Play (rock paper scissors): ")).toLowerCase();
+	var computerSelection = getComputerChoice();
+	var gamePlay = playRound(playerSelection, computerSelection);
+	if (gamePlay == 2)
+	{
+		gamePlay = playRound(playerSelection, computerSelection);
+	}
+
+	inc++;
 }
